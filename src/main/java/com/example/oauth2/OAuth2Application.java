2c4834f790b4a74add4d0cb6f1e8b26104d6c2cb
MikeQin <mike.qin@gmail.com>
2019-05-12 18:17:38 -0400

    update

--- src/main/java/com/example/oauth2/OAuth2Application.java
+++ src/main/java/com/example/oauth2/OAuth2Application.java
@@ -2,11 +2,17 @@ package com.example.oauth2;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,41 +24,71 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 public class OAuth2Application extends WebSecurityConfigurerAdapter {
+	
+	@GetMapping(path = "/user")
+	public Principal user(Principal principal) {
+		log.info("OAuth2Application /user endpoint called ...");
+		log.info("Principal Name: {}", principal.getName());
+		log.info("Principal: {}", principal);
 
-  @GetMapping(path = "/user")
-  public Principal user(Principal principal) {
-    log.info("OAuth2Application /user endpoint called ...");
-    log.info("Principal Name: {}", principal.getName());
-    log.info("Principal: {}", principal);
-    
-    return principal;
-  }
+		return principal;
+	}
+	
+	@Autowired
+	OAuth2RestTemplate oauth2Template;
+	
+	@GetMapping(path = "/access_tokens", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<String> accessTokens() {
+		log.info("/access_tokens called ...");		
+		String json = oauth2Template.getForObject("http://localhost:8081/auth/tokens", String.class);
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-        //.antMatcher("/**")
-        .authorizeRequests()
-          .antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
-          .antMatchers("/user").access("hasRole('ADMIN') and hasRole('USER')")
-          .anyRequest().authenticated()
-        .and()  // .logoutUrl("/logout")
-          .logout()
-          .logoutUrl("/logout")
-          .logoutSuccessUrl("/")
-          .invalidateHttpSession(true)
-          .deleteCookies("JSESSIONID")
-        .and()
-          .csrf() // create cookie for client and put jsessionid in cookie
-          .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
-  }
+		return ResponseEntity.ok(json);
+	}
+	
+	@GetMapping(path = "/token", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public ResponseEntity<OAuth2AccessToken> tokens() {		
+		log.info("/tokens called ...");		
+		OAuth2AccessToken token = oauth2Template.getAccessToken();
+		
+		return ResponseEntity.ok(token);
+	}
+	
+	@GetMapping(path = "/secret", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> secretResource() {
+		log.info("/secret called ...");		
+		String json = oauth2Template.getForObject("http://localhost:8081/auth/secret", String.class);
 
-  /**
-   * Main
-   * @param args
-   */
-  public static void main(String[] args) {
-    SpringApplication.run(OAuth2Application.class, args);
-  }
+		return ResponseEntity.ok(json);
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**")
+		.authorizeRequests()
+		.antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll()
+		.anyRequest().authenticated()
+		.and()
+		.exceptionHandling()
+	      .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+	    .and()
+		.logout()
+		//.logoutUrl("/logout")
+		.logoutSuccessUrl("/")
+		//.invalidateHttpSession(true)
+		//.deleteCookies("JSESSIONID")
+		//.permitAll()
+		.and()
+		.csrf() // create cookie for client and put jsessionid in cookie
+		.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+
+	/**
+	 * Main
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SpringApplication.run(OAuth2Application.class, args);
+	}
 
 }
---
+++ src/main/java/com/example/oauth2/OAuth2ClientConfig.java
@@ -0,0 +1,48 @@
+package com.example.oauth2;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+
+@Configuration
+@EnableOAuth2Client
+public class OAuth2ClientConfig {
+
+	@Autowired
+	private OAuth2ClientContext oauth2Context;
+
+	@Value("${security.oauth2.client.clientId:SampleClientId}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.clientSecret:secret}")
+	private String clientSecret;
+	
+	@Value("${security.oauth2.client.userAuthorizationUri:http://localhost:8081/auth/oauth/authorize}")
+	private String authorizeUrl;
+
+	@Value("${security.oauth2.client.accessTokenUri:http://localhost:8081/auth/oauth/token}")
+	private String tokenUrl;
+	
+	@Bean
+	public OAuth2RestTemplate restTemplate() {
+		return new OAuth2RestTemplate(resource(), oauth2Context);
+	}
+	
+	@Bean
+	protected OAuth2ProtectedResourceDetails resource() {
+		
+		AuthorizationCodeResourceDetails resource = new AuthorizationCodeResourceDetails();
+		resource.setClientId(clientId);
+		resource.setAccessTokenUri(tokenUrl);
+		resource.setUserAuthorizationUri(authorizeUrl);
+		resource.setClientSecret(clientSecret);
+		
+		return resource ;
+	}
+}
